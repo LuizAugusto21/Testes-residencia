bbab0688fe5da2ac9e2264ba3aefc4ca6397ab69
Luiz Augusto <luis2848@gmail.com>
2024-09-27 11:39:12 -0300

    Feat: implementação do CalcularCustoTotal

--- src/main/java/ecommerce/service/CompraService.java
+++ src/main/java/ecommerce/service/CompraService.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import ecommerce.entity.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +12,6 @@ import ecommerce.dto.CompraDTO;
 import ecommerce.dto.DisponibilidadeDTO;
 import ecommerce.dto.EstoqueBaixaDTO;
 import ecommerce.dto.PagamentoDTO;
-import ecommerce.entity.CarrinhoDeCompras;
-import ecommerce.entity.Cliente;
 import ecommerce.external.IEstoqueExternal;
 import ecommerce.external.IPagamentoExternal;
 import jakarta.transaction.Transactional;
@@ -72,7 +71,48 @@ public class CompraService {
 	}
 
 	public BigDecimal calcularCustoTotal(CarrinhoDeCompras carrinho) {
-		// To-Do
-		return BigDecimal.ZERO;
+		BigDecimal custoProdutos = BigDecimal.ZERO;
+		BigDecimal pesoTotal = BigDecimal.ZERO;
+
+		// Calculando o custo total dos produtos e o peso total
+		for (ItemCompra item : carrinho.getItens()) {
+			Produto produto = item.getProduto();
+			BigDecimal precoProduto = produto.getPreco();
+			BigDecimal pesoProduto = BigDecimal.valueOf(produto.getPeso());
+
+
+			custoProdutos = custoProdutos.add(precoProduto.multiply(BigDecimal.valueOf(item.getQuantidade())));
+
+			pesoTotal = pesoTotal.add(pesoProduto.multiply(BigDecimal.valueOf(item.getQuantidade())));
+		}
+
+		// Aplicar desconto
+		if (custoProdutos.compareTo(BigDecimal.valueOf(1000)) > 0) {
+			custoProdutos = custoProdutos.multiply(BigDecimal.valueOf(0.80)); // Desconto de 20%
+		} else if (custoProdutos.compareTo(BigDecimal.valueOf(500)) > 0) {
+			custoProdutos = custoProdutos.multiply(BigDecimal.valueOf(0.90)); // Desconto de 10%
+		}
+
+		BigDecimal frete = calcularFrete(pesoTotal);
+
+		if (carrinho.getCliente().getTipo() == TipoCliente.OURO) {
+			frete = BigDecimal.ZERO; //
+		} else if (carrinho.getCliente().getTipo() == TipoCliente.PRATA) {
+			frete = frete.multiply(BigDecimal.valueOf(0.50));
+		}
+
+		return custoProdutos.add(frete);
+	}
+
+	private BigDecimal calcularFrete(BigDecimal pesoTotal) {
+		if (pesoTotal.compareTo(BigDecimal.valueOf(5)) <= 0) {
+			return BigDecimal.ZERO; // Até 5kg, frete grátis
+		} else if (pesoTotal.compareTo(BigDecimal.valueOf(10)) <= 0) {
+			return pesoTotal.multiply(BigDecimal.valueOf(2)); // De 5kg até 10kg, R$ 2,00 por kg
+		} else if (pesoTotal.compareTo(BigDecimal.valueOf(50)) <= 0) {
+			return pesoTotal.multiply(BigDecimal.valueOf(4)); // De 10kg até 50kg, R$ 4,00 por kg
+		} else {
+			return pesoTotal.multiply(BigDecimal.valueOf(7)); // Acima de 50kg, R$ 7,00 por kg
+		}
 	}
 }
